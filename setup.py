e51a44862064e29d148fc7220823ae1052a3f4b7
Thomas Khyn <thomas@ksytek.com>
2014-04-10 17:57:04 +1200

    distutils > setuptools in setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,9 @@ Allows multiple apps to override the same management command in Django
 MIT license (see LICENSE.txt)
 """
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
 
-INC_PACKAGES = 'django_mcmo',  # string or tuple of strings
-EXC_PACKAGES = ()  # tuple of strings
-
-install_requires = (
-    'django>=1.6',
-)
 
 # imports __version__ variable
 exec(open('django_mcmo/version.py').read())
@@ -29,11 +23,12 @@ DEV_STATUS = {'pre': '2 - Pre-Alpha',
               'final': '5 - Production/Stable'}
 
 # setup function parameters
-metadata = dict(
+setup(
     name='django-mcmo',
     version=__version__,
     description='Allows multiple apps to override the same management ' \
                 'command in Django',
+    long_description=open(os.path.join('README.rst')).read(),
     author='Thomas Khyn',
     author_email='thomas@ksytek.com',
     url='http://bitbucket.org/tkhyn/django-mcmo',
@@ -49,38 +44,14 @@ metadata = dict(
         'Environment :: Other Environment',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities'
-    ]
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['LICENSE.txt', 'README.rst']
+    },
+    install_requires=(
+        'django>=1.6',
+    ),
+    zip_safe=False
 )
-
-
-# packages parsing from root packages, without importing sub-packages
-root_path = os.path.dirname(__file__)
-if isinstance(INC_PACKAGES, basestring):
-    INC_PACKAGES = (INC_PACKAGES,)
-
-packages = []
-excludes = list(EXC_PACKAGES)
-for pkg in INC_PACKAGES:
-    pkg_root = os.path.join(root_path, *pkg.split('.'))
-    for dirpath, dirs, files in os.walk(pkg_root):
-        rel_path = os.path.relpath(dirpath, pkg_root)
-        pkg_name = pkg
-        if (rel_path != '.'):
-            pkg_name += '.' + rel_path.replace(os.sep, '.')
-        for x in excludes:
-            if x in pkg_name:
-                continue
-        if '__init__.py' in files:
-            packages.append(pkg_name)
-        elif dirs:  # stops package parsing if no __init__.py file
-            excludes.append(pkg_name)
-
-
-def read(filename):
-    return open(os.path.join(root_path, filename)).read()
-
-setup(**dict(metadata,
-   packages=packages,
-   long_description=read('README.rst'),  # use reST in README.txt !
-   install_requires=install_requires
-))
